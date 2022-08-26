a7048b72a04117955ce860c6751abd96744cf2a2
user <user@192.168.0.30>
2022-08-27 07:00:18 +0900

    2回目のコミット

--- Jtech/src/SampleProject/Main.java
+++ Jtech/src/SampleProject/Main.java
@@ -4,9 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-	Arithmetic am = new Arithmetic();
+        Arithmetic am = new Arithmetic();
         System.out.println("足し算をします\n");
-	int ans = am.add(100, 200);
+        int ans = am.add(100, 200);
         System.out.println("答えは" + ans + "です");
+        
+        System.out.println("");
+
+        System.out.println("引き算をします\n");
+        ans = am.add(300, 200);
+        System.out.println("答えは" + ans + "です");
+
     }
 }
